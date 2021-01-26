60cce80b260aaa2306a3f9d552605c36c5c278c4
dark_PK <kakoy-to_wuk@mail.ru>
2021-01-26 18:09:00 +0300

    drgdxf\scfzcx\zxx

--- src/gdskspfdg.kt
+++ src/gdskspfdg.kt
@@ -2,7 +2,6 @@ class gdskspfdg {
     fun sum(a:Int, b:Int) = a+b
 
     fun min(a:Int, b:Int) = a-b
-    fun del(a:Int, b:Int) = a-b
-    fun mffn(a:Int, b:Int) = a-b
     fun dsl(a:Int, b:Int) = a-b
+    fun dssl(a:Int, b:Int) = a-b
 }
